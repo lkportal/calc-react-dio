7f527aac1fbbf1ff00a5dfb87f1193d39ade55ea
lkportal <lksxim@gmail.com>
2022-12-02 10:51:46 -0300

    part one

---
+++ src/components/inputs/index.jsx
@@ -0,0 +1,10 @@
+import { InputConteiner } from "./style"
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () =>{
+        return(
+         <InputConteiner>
+           <input />
+         </InputConteiner>  
+        )
+
+}
---
+++ src/components/inputs/style.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const InputConteiner = styled.div`
+    width:100%;
+    height: 75px;
+    background-color: #AAAFFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input {
+        width:100%;
+        height:75px;
+        background-color:#AAAFFF;
+        border: 0;
+        display:block;
+        direction:ltr;
+        padding:0 10px;
+        font-size: 24px;
+    font-family: 'Roboto';
+    color:#FFFFFF
+    }
+`
